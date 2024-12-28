2c87f1612faaf1571ae5e5a493c9424464e1b370
MarAntBQ <github@marantbq.dev>
2024-12-27 23:40:48 -0500

    Status: Working with console WIP: Fix issue with First Project

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string email = "hire-me@matantbq.dev";
+int age = 28;
+string userName = "Marco Antonio";
+Console.WriteLine($"Hello, my name is {userName} and I'm {age} years old. You can reach me at {email}.");
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c64418f80a427dc71b0426fe52c06b665525aba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c2bb53bd946ccece02fa8e1042642c7ee77236")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
